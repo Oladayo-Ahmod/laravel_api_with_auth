099951f5634fb8aad306d3a5a5e5c063b3bd4c08
oladayo ahmod <oladayoahmod112@gmail.com>
2022-03-08 23:33:47 +0100

    modify the api functionalities

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/post',[PostController::class,'index']); // get all the data
-Route::get('/post/{id}',[PostController::class,'show']); // get data of a specific id
-Route::post('/post',[PostController::class,'store']); // create new posts
-Route::put('/post/{id}',[PostController::class,'update']); // update  posts
-Route::delete('/post/{id}',[PostController::class,'destroy']); // delete post
+// Route::get('/post',[PostController::class,'index']); // get all the data
+// Route::get('/post/{id}',[PostController::class,'show']); // get data of a specific id
+// Route::post('/post',[PostController::class,'store']); // create new posts
+// Route::put('/post/{id}',[PostController::class,'update']); // update  posts
+// Route::delete('/post/{id}',[PostController::class,'destroy']); // delete post
+
+Route::resource('/post',PostController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
